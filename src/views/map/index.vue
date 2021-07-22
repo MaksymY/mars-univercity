5e8cbe1d2e7b175f8a86616554a26df5d46a232d
HuguesRomain <huguesromainpro@gmail.com>
2021-07-22 15:39:50 +0200

    toggle

--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,14 +1,19 @@
 <template>
 	<div class="map">
 		<h1>MAP</h1>
+		<Toggle text="test"></Toggle>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Toggle from "@/components/atoms/Toggle.vue";
 
 export default defineComponent({
 	name: "Map",
+	components: {
+		Toggle,
+	},
 });
 </script>
 
